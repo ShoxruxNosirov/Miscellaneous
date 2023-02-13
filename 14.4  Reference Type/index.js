15ca16c1ececa7af9e016788a881fa606f53c644
ShoxruxNosirov <shoxruxfriend@gmail.com>
2023-02-13 15:10:38 +0500

    Carrying

--- 14.4  Reference Type/index.js
+++ 14.4  Reference Type/index.js
@@ -1,18 +1,20 @@
-let user = {
-    name: "John",
-    hi() { console.log(this.name); },
-    bye() { console.log("Bye"); }
-};
-user.hi(); // works
-// now let's call user.hi or user.bye depending on the name
-//  (user.name == "John" ? user.hi : user.bye)(); // Error!
+function curry(func) {
+    return function curried(...args) {
+      if (args.length >= func.length) {
+        return func.apply(this, args);
+      } else {
+        return function(...args2) {
+          return curried.apply(this, args.concat(args2));
+        }
+      }
+    };
+  }
 
+  function sum(a, b, c) {
+    return a + b + c;
+  }
 
-let obj, method;
-obj = {
-    go: function () { console.log(this); }
-};
-obj.go();               // (1) [object Object]
-(obj.go)();             // (2) [object Object]
-(method = obj.go)();    // (3) undefined
-(obj.go || obj.stop)(); // (4) undefined
+  let curriedSum = curry(sum);
+  alert( curriedSum(1, 2, 3) ); // 6, still callable normally
+  alert( curriedSum(1)(2,3) ); // 6, currying of 1st arg
+  alert( curriedSum(1)(2)(3) ); // 6, full currying
